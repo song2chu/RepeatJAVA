ecdc9feb33a2e2e1c6e7a2fbc94eabc05f840cf2
tod2chu <todsawat.chutimsakul@outlook.com>
2018-08-20 14:33:35 -0300

    inculded CurrentDate

---
+++ DateAndTime/CurrentDate.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DateAndTime;
+import java.util.Date;
+/**
+ *
+ * @author safev
+ */
+public class CurrentDate {
+    public static void main(String[] args) {
+        Date Today = new Date();
+        System.out.println("Now is: "+Today);
+    }
+}
